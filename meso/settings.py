d56d3054e19888951f52588c3f20be167e64a411
stillnurs <noorsultan.mamataliev@gmail.com>
2021-01-06 18:40:31 +0600

    fixed token auth error

--- meso/settings.py
+++ meso/settings.py
@@ -63,8 +63,6 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     )
 }
 
